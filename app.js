3d963bed9d007a9385f102875fe612bc4b803168
ayeshaqadri <qadria392@gmail.com>
2025-08-26 11:33:00 +0500

    practice

--- app.js
+++ app.js
@@ -12,27 +12,59 @@
 // B = BIGINT
 // U = UNDEFINED      kuch dia hi nahi hai value me 
 
-let a = null;
-let b = 158;
-let c = true; //can also be false
-let d = BigInt("158") + BigInt("5")
-let e = "Ayesha"
-let f = Symbol("I am a nice symbol")
-let g = undefined
-console.log(a,b,c,d,e,f,g);
+// let a = null;
+// let b = 158;
+// let c = true; //can also be false
+// let d = BigInt("158") + BigInt("5")
+// let e = "Ayesha"
+// let f = Symbol("I am a nice symbol")
+// let g = undefined
+// console.log(a,b,c,d,e,f,g);
 
 // hame kese pata chalega inki type kia hai for this we need to type
-console.log(typeof e);
+// console.log(typeof e);
 
 // Objects ==> they are non primitive
 
-const items = {
-    "Ayesha" : true,
-    "Sara" : false,
-    "Sadia" : 100,
-    "Abdullah" : undefined
-}
-console.log(items["Ayesha"]);
+// const items = {
+//     "Ayesha" : true,
+//     "Sara" : false,
+//     "Sadia" : 100,
+//     "Abdullah" : undefined
+// }
+// console.log(items["Ayesha"]);
 
 
 // inka use kidhr hota hai agr ahame students list banani ho unhe call krne ke lie like just a practice code
+
+                            // DAY 2
+
+
+//                             QUISTION NO 1
+// let a = "Ayesha"
+// let b = 5
+// console.log(a + b);
+//                             QUISTION NO 2
+// let a = null
+// let b = 2008
+// console.log(typeof (a + b));
+//                             QUISTION NO 3
+// const a ={
+//     name : "Ayesha",
+//     age : 17,
+//     carrier : "Web and app develper"
+// }
+// a ['name'] = "Ayesha qadri"
+// a ['Date of birth'] = "1-05-2008" 
+
+// console.log(a);
+//                             QUISTION NO 4
+// const words = {
+//     hardwork : "a great deal of effort or endurance.",
+//     Structure:" the way something is built or organized.",
+//     Component: "An individual part or element of a larger whole"
+// }
+
+// console.log(words);
+
+// console.log("Structure");
