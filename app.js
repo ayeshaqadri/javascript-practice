26600824a1bd3cac7f89b21db89635896d735465
ayeshaqadri <qadria392@gmail.com>
2025-10-04 22:36:33 +0500

    practice

--- app.js
+++ app.js
@@ -424,13 +424,21 @@ console.log(mul +" is a squar of " + a );*/
 // let num = [123, 33, 76, 975, 4, 9, 48, 29];
 // num.sort();
 // console.log(num);
+    
 
+
+
+
+// delete from here
 // sort
 // let numb = [378, 338, 08, 975, 4, 9, 45, 29];
 // numb.sort();
 // console.log(numb);
 
-
+// sort
+// let numb = [3536, 27,  083, 975, 4, 9, 45, 29];
+// numb.sort();
+// console.log(numb);
 
 
 
